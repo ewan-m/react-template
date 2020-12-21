28bffee6af3931e3c2d9116c532a79c3dca7a3ce
Ewan Morrison <ewanmorrrison@gmail.com>
2020-12-21 14:35:55 +0000

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { render } from "react-dom";
+import "./index.scss";
+
+const App = () => (
+	<h1>Hello world!</h1>
+);
+
+render(<App />, document.getElementById("root"));
